048d2706820736e1a299487422b486523d9d9273
Kieran Hogg <kieran.hogg@gmail.com>
2015-02-22 04:47:57 +0000

    remove Document, add Message to admin

--- uploader/admin.py
+++ uploader/admin.py
@@ -7,7 +7,6 @@ admin.site.register(Syllabus)
 admin.site.register(ExamBoard)
 admin.site.register(ExamLevel)
 admin.site.register(Subject)
-admin.site.register(Document)
 admin.site.register(Resource)
 admin.site.register(Unit)
 admin.site.register(UnitTopic)
@@ -16,6 +15,7 @@ admin.site.register(Licence)
 admin.site.register(File)
 admin.site.register(Rating)
 admin.site.register(UserProfile)
+admin.site.register(Message)
 
 
 
