e0d5427b3554694d5a82f0d60733ec7a0191024f
Kieran Hogg <kieran.hogg@gmail.com>
2015-02-23 04:17:34 +0000

    add context processor for messages on all pages (doesn't work yet)

--- openteach/context_processors.py
+++ openteach/context_processors.py
@@ -1,8 +1,14 @@
 from django.conf import settings
+from uploader.models import Message
  
 def global_settings(request):
     # return any necessary values
     return {
         'SITE_NAME': settings.SITE_NAME,
         'APP_VERSION': settings.APP_VERSION
+    }
+    
+def messages(request):
+    return {
+        'messages': Message.objects.filter(),
     }
