32277d436b56acd4417fec5ffedcd61611e43527
Kieran Hogg <kieran.hogg@gmail.com>
2015-02-25 07:01:48 +0000

    swap url order to prevent admin clash

--- openteach/urls.py
+++ openteach/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'openteach.views.home', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('uploader.urls', namespace="uploader")),
     url(r'^uploader/', include('uploader.urls', namespace="uploader")),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
+
 )
