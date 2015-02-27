39622ea39650e002e7947f9a8dfc8fb13c9240a5
Kieran Hogg <kieran.hogg@gmail.com>
2015-02-27 17:57:34 +0000

    small whitespace change

--- openteach/urls.py
+++ openteach/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'openteach.views.home', name='home'),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('uploader.urls', namespace="uploader")),
+
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^uploader/', include('uploader.urls', namespace="uploader")),
     url(r'^accounts/', include('allauth.urls')),
-
 )
+
