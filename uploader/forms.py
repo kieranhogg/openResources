eea2e320bcb68842a6c4244420e7a0e746f3c5cd
Kieran Hogg <kieran.hogg@gmail.com>
2015-02-27 17:53:21 +0000

    check files that are uploaded, closes #18

--- uploader/forms.py
+++ uploader/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from django.conf import settings
+from django.template.defaultfilters import filesizeformat
+from django.utils.translation import ugettext_lazy as _
 from uploader.models import (Resource, Bookmark, File, Subject, Syllabus, Unit,
     UnitTopic)
-
 
 
 class BookmarkStageOneForm(forms.ModelForm):
@@ -25,6 +26,23 @@ class FileStageOneForm(forms.ModelForm):
         widgets = {'description': forms.Textarea()}
         fields = ('title', 'file', 'description', 'type', 'i_am_the_author', 
             'author', 'author_link', 'licence')
+    
+    def clean(self):
+        valid = super(FileStageOneForm, self).is_valid()
+        data = self.cleaned_data['file']
+        try:
+            if data.content_type in settings.UPLOAD_FILE_TYPES:
+                if data.size > settings.UPLOAD_FILE_MAX_SIZE:
+                    raise forms.ValidationError(_('File size must be under %s. Current file size is %s.') % (filesizeformat(settings.UPLOAD_FILE_MAX_SIZE), filesizeformat(data.size)))
+                else:
+                    raise forms.ValidationError(_('File type (%s) is not supported.') % data.content_type)
+        except AttributeError:
+            pass
+
+
+    
+ 
+
 
         
 class ResourceStageTwoForm(forms.ModelForm):
