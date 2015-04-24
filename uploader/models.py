2b3b7025fd804dc25a7f852a696e42d73a8ffee2
Kieran Hogg <kieran.hogg@gmail.com>
2015-04-24 16:23:04 +0000

    add reverse import

--- uploader/models.py
+++ uploader/models.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.models import User
@@ -7,6 +8,7 @@ from django.db import models
 from django.db.models.signals import pre_delete, post_save
 from django.dispatch.dispatcher import receiver
 from django.utils.safestring import mark_safe
+
 #from taggit_autosuggest.managers import TaggableManager
 
 logging.basicConfig()
