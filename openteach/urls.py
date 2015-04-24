07973a867701ca235453c50381d95fdfef98caa2
Kieran Hogg <kieran.hogg@gmail.com>
2015-04-24 16:48:58 +0000

    use basic searchview

--- openteach/urls.py
+++ openteach/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-#from haystack.generic_views.SearchView
-#from haystack.forms import HighlightedSearchForm
+from haystack.forms import SearchForm
+from haystack.views import SearchView
 
 urlpatterns = patterns('',
     (r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^search/', include('haystack.urls')),
+    url(r'^search/$', SearchView(form_class=SearchForm)),
     url(r'', include('uploader.urls', namespace="uploader")),
     url(r'^uploader/', include('uploader.urls', namespace="uploader")),
     #(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
