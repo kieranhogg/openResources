925686924190c1f4d1c3bf7c24ac3f397d3dadcf
Kieran Hogg <kieran.hogg@gmail.com>
2015-04-26 06:57:17 +0000

    conditional urls

--- openteach/urls.py
+++ openteach/urls.py
@@ -1,15 +1,29 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from haystack.forms import SearchForm
-from haystack.views import SearchView
+
+from openteach import settings
+
+if not settings.DEBUG:
+    from haystack.forms import HighlightedSearchForm
+    from haystack.views import SearchView
 
 urlpatterns = patterns('',
-    (r'^grappelli/', include('grappelli.urls')),
+    #(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^search/$', SearchView(form_class=HighlightedSearchForm)),
     url(r'', include('uploader.urls', namespace="uploader")),
     url(r'^uploader/', include('uploader.urls', namespace="uploader")),
-    #(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
 )
 
+if not settings.DEBUG:
+    additional_patterns = patterns(
+        url(r'^search/$', SearchView(form_class=HighlightedSearchForm)),
+    )
+    urlpatterns += additional_patterns 
+
+additional_patterns = patterns(
+    url(r'', include('uploader.urls', namespace="uploader")),
+    url(r'^uploader/', include('uploader.urls', namespace="uploader")),
+)
+
+urlpatterns += additional_patterns
