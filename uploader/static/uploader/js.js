6aac87e95d9679f70acc6656717dbef056132c7a
Kieran Hogg <kieran.hogg@gmail.com>
2015-05-07 07:00:38 +0000

    fix linking js

--- uploader/static/uploader/js.js
+++ uploader/static/uploader/js.js
@@ -6,64 +6,75 @@ $(function() {
     $('select[name=unit_topic]')
                 .empty().append($('<option></option>'))
                 
-});
-// FIXME move this and functionise
-// get syllabuses for subject
-$(document).ready(function(){
-    $('select[name=id_subject]').change(function(){
+    $('select[name=subject]').change(function(){
             subject_id = $(this).val();
             $.getJSON("/lookup/syllabus/" + subject_id, null, function(data) {
             $.each(data, function(key, value) {   
-             $('select[name=id_syllabus]')
+             $('select[name=syllabus]')
                 .append($('<option>', { value : key })
                 .text(value)); 
             });
         });
     });
-});
+
 
 // get units for syllabus
-$(document).ready(function(){
     $('select[name=syllabus]').change(function(){
             syllabus_id = $(this).val();
             $.getJSON("/lookup/unit/" + syllabus_id, null, function(data) {
             $('select[name=id_unit]')
                 .empty().append($('<option></option>'))
             $.each(data, function(key, value) {   
-             $('select[name=id_unit]')
+             $('select[name=unit]')
                 .append($('<option>', { value : key })
                 .text(value)); 
             });
         });
     });
-});
+
 
 // get unit topics for unit
-$(document).ready(function(){
-    $('select[name=id_unit]').change(function(){
+
+    $('select[name=unit]').change(function(){
             unit_id = $(this).val();
             $.getJSON("/lookup/unit_topic/" + unit_id, null, function(data) {
-            $('select[name=id_unit_topic]')
-                .empty().append($('<option><option>'))
-            $.each(data, function(key, value) {   
-             $('select[name=id_unit_topic]')
-                .append($('<option>', { value : key })
-                .text(value)); 
-            });
+            // $('select[name=unit_topic]')
+            //     .empty().append($('<option><option>'))
+            
+            var self = this;
+            this.section = false;
+            count = 0;
+            $.each(data, function(key, value) {
+                if (value.section) {
+                    if (!self.section || value.section != self.section) {
+                        if (count != 0) {
+                            $('select[name=unit_topic]').append('</optgroup>');
+                        }
+                        count++;
+                        $('select[name=unit_topic]').append('<optgroup label=" &gt; ' + value.section + '">');
+                        self.section = value.section;
+                    }
+                }
+                 $('select[name=unit_topic]')
+                    .append($('<option>', { value : value.id })
+                    .text(value.unit_topic)); 
+                });
         });
     });
-});
+
 
 // we set subject on the previous page, fill this in and pre-fill syllabuses
-$(document).ready(function(){
+
     var subject = '{{subject}}';
-    $('[name=subject]').val(subject);
-    subject_id = $('[name=id_subject]').val();
-    $.getJSON("/lookup/syllabus/" + subject_id, null, function(data) {
-    $.each(data, function(key, value) {   
-     $('select[name=syllabus]')
-        .append($('<option>', { value : key })
-        .text(value)); 
+    if (subject != "") {
+        $('[name=subject]').val(subject);
+        subject_id = $('[name=id_subject]').val();
+        $.getJSON("/lookup/syllabus/" + subject_id, null, function(data) {
+        $.each(data, function(key, value) {   
+         $('select[name=syllabus]')
+            .append($('<option>', { value : key })
+            .text(value)); 
+            });
         });
-    });
+    }
 });
